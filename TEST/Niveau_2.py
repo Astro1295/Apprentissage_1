67989dc16124b15401b0124575a90780f185b6a7
amirouche <amiroucherahani@gmail.com>
2017-05-08 18:00:31 +0200

    rien

--- TEST/Niveau_2.py
+++ TEST/Niveau_2.py
@@ -1,3 +1,15 @@
 chaine = "AMIROUCHE"
 chaine = chaine.lower()
 print(chaine)
+
+nom = "rahani"
+prenom = "amirouche"
+
+print("je suis monsieur {0}  {1}".format(prenom, nom))
+
+info = """{nom},{prenom},{adresse},({pays})""".format(nom= "RAHANI", prenom ="Amirouche", adresse = "5 rue des aubevoys, 85800 Cergy", pays = "France")
+print(info)
+
+ma_list = ["1", "2"]
+ma_list.append(3)
+print(ma_list.__len__())
