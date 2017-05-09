54dae9a01196d8a5de0a94fd163add9557143024
amirouche <amiroucherahani@gmail.com>
2017-05-09 11:10:27 +0200

    Petite modif

--- TEST/Niveau_2.py
+++ TEST/Niveau_2.py
@@ -10,6 +10,10 @@ print("je suis monsieur {0}  {1}".format(prenom, nom))
 info = """{nom},{prenom},{adresse},({pays})""".format(nom= "RAHANI", prenom ="Amirouche", adresse = "5 rue des aubevoys, 85800 Cergy", pays = "France")
 print(info)
 
-ma_list = ["1", "2"]
-ma_list.append(3)
+ma_list = [1, 2,3,3,1, 2,3,3]
 print(ma_list.__len__())
+
+i=0
+for i, elt in enumerate(ma_list):
+    print("\n",ma_list[i])
+    i+=1
