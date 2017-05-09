1181cdd55b45bf5e3a0099e9bff734bd27d58452
amirouche <amiroucherahani@gmail.com>
2017-05-09 18:42:29 +0200

    Ajout du fichier applicatin achat en ligne avec info users

--- TEST/Niveau_2.py
+++ TEST/Niveau_2.py
@@ -1,28 +1,34 @@
-def afficher_flottant(flottant):
-    """Fonction prenant en paramètre un flottant et renvoyant une chaîne de caractères représentant la troncature de ce nombre. La partie flottante doit avoir une longueur maximum de 3 caractères.
+import os
+import pickle
+
+path = "C:/Users/BQXP7653/git/Apprentissage_1"
+current_dir = os.getcwd()
+print(current_dir)
+os.chdir(path)
+
+print("****Bienvenue sur notre application de vente en ligne *****")
+# deande d'information sur le user
+
+sexe_user = input("Votre sexe H ou F ? ")
+nom_user = input("C'est quoi votre nom ? \n")
+prenom_user = input("C'est quoi votre prenom ? \n")
+adresse_user = input("C'est quoi votre adresse ? \n")
+age_user = input("Quel est votre age ? \n")
+
+#traitement des information en base
+
+sexe_user= sexe_user.lower()
+
+if sexe_user == "h":
+    sexe_user = "homme"
+else:
+    sexe_user = "femme"
+# ecriture des infoamtion sur le fichier usr_info.txt
+user_info ={"sexe":sexe_user,"nom":nom_user,"prenom":prenom_user,"adresse":adresse_user,"age":age_user}
 
 
-    De plus, on va remplacer le point décimal par la virgule"""
+fichier = open("path", "a")
+for x,y in user_info.items():
+    ecrire= fichier.write("{0}:{1}.\n" .format(x,y))
+fichier.close()
 
-    if type(flottant) is not float:
-        raise TypeError("Le paramètre attendu doit être un flottant")
-
-    flottant = str(flottant)
-
-    partie_entiere, partie_flottante = flottant.split(".")
-
-    # La partie entière n'est pas à modifier
-
-    # Seule la partie flottante doit être tronquée
-
-    return ",".join([partie_entiere, partie_flottante[:3]])
-
-print(afficher_flottant(12.2222222))
-ma_list = [1,2,4,5,2]
-
-ma_list.sort()
-
-ma_liste1 = sorted(ma_list)
-
-print(ma_list)
-print(ma_liste1)
