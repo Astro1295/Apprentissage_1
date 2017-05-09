094ff4f8541da5f408e8e4a15aad317c285081bc
amirouche <amiroucherahani@gmail.com>
2017-05-09 14:02:22 +0200

    Travail sur les liste et les insertions

--- TEST/Niveau_2.py
+++ TEST/Niveau_2.py
@@ -1,19 +1,28 @@
-chaine = "AMIROUCHE"
-chaine = chaine.lower()
-print(chaine)
+def afficher_flottant(flottant):
+    """Fonction prenant en paramètre un flottant et renvoyant une chaîne de caractères représentant la troncature de ce nombre. La partie flottante doit avoir une longueur maximum de 3 caractères.
 
-nom = "rahani"
-prenom = "amirouche"
 
-print("je suis monsieur {0}  {1}".format(prenom, nom))
+    De plus, on va remplacer le point décimal par la virgule"""
 
-info = """{nom},{prenom},{adresse},({pays})""".format(nom= "RAHANI", prenom ="Amirouche", adresse = "5 rue des aubevoys, 85800 Cergy", pays = "France")
-print(info)
+    if type(flottant) is not float:
+        raise TypeError("Le paramètre attendu doit être un flottant")
 
-ma_list = [1, 2,3,3,1, 2,3,3]
-print(ma_list.__len__())
+    flottant = str(flottant)
 
-i=0
-for i, elt in enumerate(ma_list):
-    print("\n",ma_list[i])
-    i+=1
+    partie_entiere, partie_flottante = flottant.split(".")
+
+    # La partie entière n'est pas à modifier
+
+    # Seule la partie flottante doit être tronquée
+
+    return ",".join([partie_entiere, partie_flottante[:3]])
+
+print(afficher_flottant(12.2222222))
+ma_list = [1,2,4,5,2]
+
+ma_list.sort()
+
+ma_liste1 = sorted(ma_list)
+
+print(ma_list)
+print(ma_liste1)
